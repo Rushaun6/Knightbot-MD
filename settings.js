00cfe5d7f72a5680ef3b68db1a2d00d50d5a9dc1
Rushaun6 <166856723+Rushaun6@users.noreply.github.com>
2026-02-18 21:00:08 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Knit Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Knit Bot",
+  botOwner: 'Adam', // Your name
+  ownerNumber: '18764526429', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
